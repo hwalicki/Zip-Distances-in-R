5cb7bca02977622b501b3e4578b1a6e14b4c423b
hwalicki <hwalicki@gmail.com>
2016-01-10 20:51:35 -0700

    Updated to calc distance using lat/long & Haversine formula

--- Zip Code Distance Calcs.R
+++ Zip Code Distance Calcs.R
@@ -1,95 +1,60 @@
 ################### GET DISTANCES BETWEEN ZIPS IN MULTIPLE REGIONS ############
-library(XML)
 
-basefp <- "C:\\Users\\Heather\\Documents\\R\\Other\\Zip Distances\\"
-### Load data (Original Source: https://www.aggdata.com/node/86)
-orig_dat <- read.csv(paste(basefp,"us_postal_codes.csv",sep=""))
+### Load functions
+baseFp <- "C:\\Users\\Heather\\Documents\\R\\Other\\Zip-Distances-in-R\\"
+setwd(baseFp)
+source("zipFuns.r")
 
-desired_regions <- c("Delaware","Rhode Island") #For ease, just do a few small states
-dat <- orig_dat[(orig_dat$State %in% desired_regions),]
+### Load data 
+# Original Source: https://www.aggdata.com/node/86
+origDat <- read.csv(paste(baseFp, "us_postal_codes.csv", sep=""))
+
+desiredRegions <- c("Delaware","Rhode Island") #For ease, just do a few small states
+dat <- origDat[(origDat$State %in% desiredRegions),]
 gc()
 
-# Sort
-regions <- as.vector(unique(dat$State)) #this can be any region in dataset
-ct <- sapply(1:length(regions),function(h) {
+### Sort
+regions <- as.vector(unique(dat$State)) #this can be any partition in dataset
+ct <- sapply(1:length(regions), function(h) {
   reg <- regions[h]
   len <- nrow(dat[dat$State == reg,])
   return(len)
 })
-srt <- as.data.frame(cbind(regions,ct))
+srt <- as.data.frame(cbind(regions, ct))
 srt$ct <- as.numeric(as.character(srt$ct))
-srtd <- srt[with(srt,order(ct,regions)),]
+srtd <- srt[with(srt, order(ct, regions)),]
 regions <- as.character(srtd$regions) # Now regions will go in asc computational effort order
 gc()
 
-## Get data frames & Save
-j <- 1
-cutregs <- lapply(1:length(regions), function(i) {
+
+### Create combos & Calculate distances
+df <- data.frame(Region=character(), Zip1=integer(), Zip2=integer(), Zip1Lat=double(), Zip1Long=double(),
+                      Zip2Lat=double(), Zip2Long=double(), DistanceInMiles=double())
+
+calcDistances <- lapply(1:length(regions), function(i) {
   
   region <- regions[i]
-  reg <- dat[dat$State == region,][1]
-  df <- as.data.frame(apply(combn(unlist(reg),2),1,rev)) #unlist, get combinations, reverse matrix, & make df
-  colnames(df) <- c("Zip1","Zip2")
-  df$Region <- region
+  reg <- dat[dat$State == region,][c(1,6,7)]
+  reg$ZipLatLong <- paste(reg$Postal.Code, reg$Latitude, reg$Longitude)
+  
+  combos <- apply(combn(unlist(reg$ZipLatLong),2),1,rev) #Get combinations/reverse matrix
+  combinations <- cbind(read.table(text = combos[,1], sep=" ", colClasses = "character"),
+                        read.table(text = combos[,2], sep=" ", colClasses = "character"))
+  colnames(combinations) <- c("Zip1", "Zip1Lat", "Zip1Long", "Zip2", "Zip2Lat", "Zip2Long")
+  combinations$Region <- region
   gc()
   
-  ## Break down file (necessary when dfs get very large)
-  sub_num <- max(round(nrow(df)/15000,0),1)
-  sub_nrow <- round(nrow(df)/sub_num)  # ~15000
-  saves <- lapply(1:sub_num, function(k) {
-    
-    if (k==1) {dfs <- df[1:sub_nrow,]}
-    if (k>1 & k<sub_num) {dfs <- df[(sub_nrow*(k-1)+1):(sub_nrow*k),]}
-    if (k==sub_num && sub_num>1) {dfs <- df[(sub_nrow*(k-1)+1):nrow(df),]}
-    
-    fp <- paste(basefp,"ZipCombos",j,".rdata", sep="")
-    save(dfs,file=fp) ## Save down
-    j <<- j + 1
-    return()
-  })
+  # Calc Distance
+  combinations$DistanceInMiles <- getDistances(nrow(combinations), combinations$Zip1Lat, combinations$Zip1Long, 
+                      combinations$Zip2Lat, combinations$Zip2Long)
+  
+  df <<- rbind(df, combinations)
   print(i)
-  return()
-}) 
-
-
-
-## Loop for distances
-runLoop <- sapply(1:2, function(h) { # Specify iteration through mutliples files (ex 1:15)
-  
-  zipfp <- paste(basefp,"ZipCombos",h,".rdata", sep="") #filepath
-  load(zipfp)
-  currentRegion <- dfs$Region[1]
-  
-  ## Grab distances
-  getDist <- sapply(1:nrow(dfs), function(g) {
-    zip1 <- dfs$Zip1[g] #grab first zip
-    if (nchar(zip1)==4) {zip1 <- paste("0",zip1,sep="")} #Add leading zeros for east coast zips
-    zip2 <- dfs$Zip2[g] #grab second zip
-    if (nchar(zip2)==4) {zip2 <- paste("0",zip2,sep="")}
-    url <- paste("http://maps.google.com/maps?f=d&source=s_d&saddr=",zip1,"&daddr=",zip2,sep="") #create URL
-    dist <- tryCatch(dist <- as.numeric(strsplit(readHTMLList(url)[[5]][[1]]," ")[[1]][[1]]),
-                     error=function(e){  #data munge results & error catch
-                       dist <- NA
-                       return(dist)})
-    #globalVar <<- g
-    print(g) #turn off if real-time output undesired
-    gc()
-    return(dist)    
-  }) 
-  gc()
-  
-  dfs$DrivingDistanceMi <- getDist
-  dfs <- dfs[c(1,2,4,3)]
-  
-  fps <- paste(basefp,"ZipOutputs ",currentRegion," ",h,".csv", sep="") #save to filepath
-  write.csv(dfs,fps)  #Save CSV
-  print(paste("CSV #",h,"saved"))
-  
-  ## If iterating through heavy data, use restart below
-  #system("R --Wooden Christmas-Tree") #Restart R (user version)
-  
-  return()
-  
 })
 
-#q("no") #close w/o save
+### Break down file & Save
+# Breakdown necessary if df > 1M rows for Excel
+maxNum = 1000000
+subNum <- max(round(nrow(df)/maxNum,0),1)
+subNrow <- round(nrow(df)/subNum)  # ~1M
+saves <- cutAndSaveDfs(subNum, subNrow, df, baseFp)
---
+++ ZipFuns.R
@@ -0,0 +1,70 @@
+################## ZIP DISTANCE FUNCTIONS ###################
+
+## Distances
+getDistances <- function(rownum, lat1s, long1s, lat2s, long2s) {
+  
+  distances <- sapply(1:rownum, function(h) {
+    getDist(as.numeric(lat1s[h]), as.numeric(long1s[h]), as.numeric(lat2s[h]), as.numeric(long2s[h]))
+  })
+
+  return(distances)
+}
+
+
+## Haversine Formula
+getDist <- function(lat1, long1, lat2, long2) {
+  
+  earthRadiusMi <- 3959
+  deltaLat <- (lat2 - lat1)
+  deltaLong <- (long2 - long1)
+  a <- sin(deltaLat/2)^2 + cos(lat1) * cos(lat2) * sin(deltaLong/2)^2
+  b <- 2 * asin(min(1,sqrt(a)))
+  dist = earthRadiusMi * b
+  
+  gc()
+  return(dist)
+}
+
+
+## Slice large df
+cutDf <- function(totalNum, subNum, subNrow, df) {
+  
+  if (totalNum == 1)
+    finalDf <- df[1:subNrow,]
+  
+  if (totalNum>1 && totalNum<subNum)
+    finalDf <- df[(subNrow*(k-1)+1):(subNrow*k),]
+  
+  if (subNum>1 && totalNum==subNum)
+    finalDf <- df[(subNrow*(k-1)+1):nrow(finalDf),]
+  
+  return(finalDf)
+}
+
+
+## Breakdown dfs
+cutAndSaveDfs <- function(subNum, subNrow, df, baseFp) {
+  
+  finalSaves <- lapply(1:subNum, function(k) {
+    
+    finalDf <- cutDf(k, subNum, subNrow, df)
+    finalDf <- finalDf[c(7,1,4,2,3,5,6,8)]
+    
+    fp <- paste(baseFp, "ZipOutputs", k, ".csv", sep="")
+    write.csv(finalDf, file=fp, row.names=F) # Save
+    print(paste("CSV #", k, " saved"))
+  })
+  
+}
+
+
+## Add leading 0s for east coast zips 
+# [OBSOLETE]
+padZip <- function(zip) {
+  
+  charNum <- nchar(zip)
+  if ((charNum) < 5)
+    zip <- paste(paste(rep("0",5-nchar(zip)),collapse=""), zip, sep="")
+  
+  return(zip)
+}
